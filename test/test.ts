4cf51ce1e1dd0392763864ee76c02e4fada7e44a
Matt Johnston <m@ttjohnston.com>
2024-02-01 17:32:41 -0400

    tests

---
+++ test/test.ts
@@ -0,0 +1,20 @@
+import { inflateResponse } from "../mod.ts";
+import { assertEquals } from "https://deno.land/std/assert/assert_equals.ts";
+import { resolve as resolvePath } from "https://deno.land/std/path/mod.ts";
+
+// correct content is "hello world"
+const pathToCorrect = resolvePath("./test/output/correct.txt");
+console.log("pathToCorrect", pathToCorrect);
+const correctContents = Deno.readTextFileSync(pathToCorrect);
+
+Deno.test(async function inflateGZipTest() {
+  const pathToInput = resolvePath("./test/input/hello-world.gz");
+  const response = await fetch(`file://${pathToInput}`);
+  const inflateDestination = "./test/output/hello-world-gzip.txt";
+  await inflateResponse(response, inflateDestination, "gzip");
+  const inflatedContents = Deno.readTextFileSync(
+    resolvePath(inflateDestination),
+  );
+  assertEquals(inflatedContents, correctContents);
+  Deno.removeSync(resolvePath(inflateDestination));
+});
